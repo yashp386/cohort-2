0b23aebff869b1b1f020c93178b30e09c4e8b21b
yashp386 <yashp30386@gmail.com>
2024-09-22 22:54:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app=express();                //compulsory lines
 app.listen(3000);                   //compulsory lines
 
 app.get("/", function(req,res){
-    res.send("hi there");       
+    res.send("hi there https://www.youtube.com/watch?v=N5jd73O9M7M&pp=0AUB ");
+           
 })
 
 //basic http server code above
