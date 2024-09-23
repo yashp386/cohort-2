03569c02a2edc372d4f949758e4c6cbcbbcb32e0
yashp386 <yashp30386@gmail.com>
2024-09-23 06:37:31 +0530

    another resource

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const express= require("express"); //compulsory lines
 
 const app=express();                //compulsory lines
 
+app.listen(3001);                   //compulsory lines
+
 function sum(n){                    //add query parameters
     let ans = 0;
     for(let i=1;i<=n;i++){
         ans = ans+i;
     }
+    return ans;
 }                  
 
 app.get("/", function(req,res){
-    const n=req.query.n              // to catch the query parameters or to take input from user
+    const n=req.query.n   ;           // to catch the query parameters or to take input from user
     const ans=sum(n);
     res.send("hi there ");
            
 })
-app.listen(3000);       //compulsory lines
+
 //basic http server code above
