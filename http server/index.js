c01ba265b62d98616d26cde589497fb265785992
yashp386 <yashp30386@gmail.com>
2024-09-23 06:50:51 +0530

    n

---
+++ http server/index.js
@@ -0,0 +1,11 @@
+//http in node
+const { Console } = require("console");
+const http=require("http");
+
+const myserver=http.createServer((req,res) => {
+    console.log("new request");
+    res.end("hello from server");
+    
+});
+
+myserver.listen(8000, () => console.log("server started"));
